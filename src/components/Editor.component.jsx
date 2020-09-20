440ec1274b48c4c1bb733e130f9eac6e3aa29199
KishoreNewton <kishorenke@gmail.com>
2020-09-20 14:19:30 +0530

    added autocomplte feature

--- src/components/Editor.component.jsx
+++ src/components/Editor.component.jsx
@@ -10,6 +10,18 @@ import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css'
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/javascript-hint';
+import 'codemirror/addon/hint/show-hint.css';
+import 'codemirror/addon/hint/anyword-hint';
+import 'codemirror/keymap/sublime';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/fold/foldcode';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/fold/comment-fold';
+import 'codemirror/addon/fold/foldgutter.css';
 
 function Editor({ title, language, value, onChange, theme }) {
 
@@ -42,7 +54,19 @@ function Editor({ title, language, value, onChange, theme }) {
                 mode: language,
                 lint: true,
                 lineNumbers: true,
-                theme: defaultOption
+                theme: defaultOption,
+                lineWrapping: true,
+                smartIndent: true,
+                lineNumbers: true,
+                foldGutter: true,
+                gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+                autoCloseTags: true,
+                keyMap: 'sublime',
+                matchBrackets: true,
+                autoCloseBrackets: true,
+                extraKeys: {
+                  'Ctrl-Space': 'autocomplete'
+                }
             }} />
         </div>
     )
